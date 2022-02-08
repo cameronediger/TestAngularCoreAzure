49fab9e1cb525d9e04817d18a074836d6ba37e11
Cameron Ediger <cameron.ediger@gmail.com>
2022-02-08 16:46:45 -0600

    Update angular unit tests and github workflows

--- TestAngularCore/ClientApp/src/app/nav-menu/environment-header/environment-header.component.spec.ts
+++ TestAngularCore/ClientApp/src/app/nav-menu/environment-header/environment-header.component.spec.ts
@@ -1,19 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonService } from '../../services/common.service';
 import { EnvironmentHeaderComponent } from './environment-header.component';
 
 describe('EnvironmentHeaderComponent', () => {
+  let mockCommonService;
   let component: EnvironmentHeaderComponent;
   let fixture: ComponentFixture<EnvironmentHeaderComponent>;
 
   beforeEach(async(() => {
+    mockCommonService = jasmine.createSpyObj(['environment'])
     TestBed.configureTestingModule({
-      declarations: [ EnvironmentHeaderComponent ]
+      declarations: [EnvironmentHeaderComponent],
+      providers: [
+        { provide: CommonService, useValue: mockCommonService }
+      ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    mockCommonService.environment.and.returnValue("Development");
     fixture = TestBed.createComponent(EnvironmentHeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- TestAngularCore/ClientApp/src/app/services/common.service.spec.ts
+++ TestAngularCore/ClientApp/src/app/services/common.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { CommonService } from './common.service';
 
 describe('CommonService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: CommonService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    service = TestBed.inject(CommonService);
+  });
 
   it('should be created', () => {
-    const service: CommonService = TestBed.get(CommonService);
     expect(service).toBeTruthy();
   });
 });
